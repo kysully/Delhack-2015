799aeb5627b6b884b1bb762b02e2a6748e8d4d92
jer-keel <jj.of.keeler@gmail.com>
2015-11-14 15:41:35 -0500

    Add routes for all pages

--- src/server/api_router.js
+++ src/server/api_router.js
@@ -1,7 +1,45 @@
 var router = require('express').Router();
+    pg     = require('pg');
 
 router.get('/', function(req, res, next) {
   res.json({"message": "welcome"});
 })
 
 module.exports = router;
+
+// Use pg client pool for better efficiency
+// pg.connect(dbh.conString, function(err, client, done) {
+//   if(dbh.handleError(err, client, done, res)) return;
+//
+//   // Create prepared statement for repeated use
+//   // Grab the latest 5 posts from the database and return their summary
+//   // information
+//   q = client.query({ name:'get_posts', text: 'SELECT id, title, summary, ' +
+//   'created_at, tags FROM summary_posts ' +
+//   'ORDER BY created_at DESC LIMIT 5 OFFSET $1;', values: [0] });
+//
+//   q.on('row', function(row, result) {
+//     result.addRow(row);
+//   });
+//
+//   q.on('err', function(err) {
+//     dbh.handleError(err, client, done, res);
+//   });
+//
+//   q.on('end', function(result) {
+//     done(client);
+//
+//     // Format the post summaries and add it to the data to be passed to the
+//     // view
+//     var formatted_result = format_summary_result(result.rows);
+//
+//     data = {
+//       posts: formatted_result,
+//       nav_class: '',
+//       page_num: 1
+//     };
+//
+//     // Generate the sidebar content
+//     create_sidebar(req, res, next, data);
+//   });
+// });
--- src/server/main_router.js
+++ src/server/main_router.js
@@ -1,7 +1,43 @@
 var router = require('express').Router();
 
+// STATUS: Un-implemented
 router.get('/', function(req, res, next) {
-  res.render('welcome.html');
+  res.render('home.html');
 })
+
+// STATUS: Un-implemented
+router.get('/live', function(req, res, next) {
+  res.render('live.html');
+});
+
+// STATUS: Un-implemented
+router.get('/historic', function(req, res, next) {
+  res.render('historic.html');
+});
+
+// STATUS: Un-implemented
+router.get('/res', function(req, res, next) {
+  res.render('res_list.html');
+});
+
+// STATUS: Un-implemented
+router.get('/res/:rid', function(req, res, next) {
+  res.render('res', {});
+});
+
+// STATUS: Un-implemented
+router.get('/res/:rid/admin', function(req, res, next) {
+  res.render('res_admin', {});
+});
+
+// STATUS: Un-implemented
+router.get('/flashdeals', function(req, res, next) {
+  res.render('flashdeals.html');
+});
+
+// STATUS: Un-implemented
+router.get('/flashdeals/:fid', function(req, res, next) {
+  res.render('flashdeal.html');
+});
 
 module.exports = router;
