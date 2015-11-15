67f8d2253dd95f8295e8dae80963a0a8b7ebf3cc
kysully <kmsully@udel.edu>
2015-11-15 04:50:59 -0500

    Skeleton for littles law
    littles law is cool

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ server.listen(process.env.PORT || port, process.env.IP || '0.0.0.0', function() 
   var address = server.address();
   console.log('Server is now started on ', address.address + ':' + address.port);
 });
+
+//find the shortest average wait and use that for the timeout
+function littlesLaw()
+{
+   //apply the forumla here
+   setTimeout(littlesLaw, 60000);//once per minute?
+}
+
+littlesLaw();
